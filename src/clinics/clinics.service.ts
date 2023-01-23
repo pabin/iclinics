4a7efc7ef0763fada454fb51b2158601af94560d
Pabin R Luitel <pabin2050@gmail.com>
2023-01-23 17:16:34 +0545

    add methods to fetch dental and vet clinics from third party API

--- src/clinics/clinics.service.ts
+++ src/clinics/clinics.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 import { FindClinicsDto } from './dto/find-clinics.dto';
+import { dentalClinicsURL, vetClinicsURL } from './apis/clinics.api';
 
 @Injectable()
 export class ClinicsService {
+  async getDentalClinics() {
+    try {
+      const dentalClinics = await axios.get(dentalClinicsURL);
+      return dentalClinics.data;
+    } catch (err) {
+      // returning empty array because another api might be working
+      return [];
+    }
+  }
+
+  async getVetClinics() {
+    try {
+      const vetClinics = await axios.get(vetClinicsURL);
+      return vetClinics.data;
+    } catch (err) {
+      return [];
+    }
+  }
+
   async findClinics(findClinicsDto: FindClinicsDto) {
     return 'return clinics array';
   }
