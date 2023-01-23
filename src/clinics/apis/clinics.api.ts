2ad38c8064610f816f758952309a551237e30b85
Pabin R Luitel <pabin2050@gmail.com>
2023-01-23 17:09:32 +0545

    add clinics API endpoints

---
+++ src/clinics/apis/clinics.api.ts
@@ -0,0 +1,5 @@
+export const dentalClinicsURL =
+  'https://storage.googleapis.com/scratchpay-code-challenge/dental-clinics.json';
+
+export const vetClinicsURL =
+  'https://storage.googleapis.com/scratchpay-code-challenge/vet-clinics.json';
